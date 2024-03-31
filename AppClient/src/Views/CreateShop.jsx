8065e532e5af4fcb533c51336d61ad95c5c8d8cd
Danny8rg <daromachine@gmail.com>
2024-03-31 00:58:03 -0500

    creacion de tiendas calidaciones

--- AppClient/src/Views/CreateShop.jsx
+++ AppClient/src/Views/CreateShop.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import Axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./CreateShop.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
 import Cookies from "js-cookie";
@@ -16,125 +15,46 @@ function CreateShop() {
   const [shopMail, setShopMail] = useState("");
   const [shopAdress, setShopAdress] = useState("");
   const [shopComments, setShopComments] = useState("");
-  const [editar, setEditar] = useState(false);
-  const [shopsList, setShopsList] = useState([]);
-  const [shopId, setShopId] = useState("");
-  const [shopOwner, setShopOwner] = useState("");
-  const [shopImgUrl, setShopImgUrl] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
 
   const navigate = useNavigate();
   const { globalShopId, setGlobalShopId } = useContext(ShopContextValues);
 
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validateShopName = (name) => {
+    return /^[A-Z].*/.test(name);
+  };
+
+  const validatePhoneNumber = (phoneNumber) => {
+    return /^\d{10}$/.test(phoneNumber);
+  };
+
   const addShop = () => {
+    // Aquí se puede agregar la lógica para validar los campos antes de enviar la solicitud
     const formData = new FormData();
     formData.append("file", selectedFile);
     formData.append("shopName", shopName);
     formData.append("shopTell", shopTell);
     formData.append("shopMail", shopMail);
     formData.append("shopAdress", shopAdress);
-    formData.append("shopOwner", shopOwner);
     formData.append("shopComments", shopComments);
 
     Axios.post("http://localhost:3000/createShop", formData)
       .then(() => {
-        getShops();
-        limpiarCampos();
         alert("Tienda registrada");
+        limpiarCampos();
       })
       .catch((error) => {
         console.error("Error al enviar la solicitud:", error);
       });
   };
 
-  const updateShop = () => {
-    // Almacena los valores originales antes de la actualización
-    const originalShopName =
-      shopsList.find((val) => val.shopId === shopId)?.shopName || "";
-    const originalShopTell =
-      shopsList.find((val) => val.shopId === shopId)?.shopTell || "";
-    const originalShopMail =
-      shopsList.find((val) => val.shopId === shopId)?.shopMail || "";
-    const originalShopAdress =
-      shopsList.find((val) => val.shopId === shopId)?.shopAdress || "";
-    const originalShopComments =
-      shopsList.find((val) => val.shopId === shopId)?.shopComments || "";
-
-    const confirmationMessage = [
-      `Nombre: ${originalShopName} -> ${shopName}`,
-      `Teléfono: ${originalShopTell} -> ${shopTell}`,
-      `Correo Electrónico: ${originalShopMail} -> ${shopMail}`,
-      `Dirección: ${originalShopAdress} -> ${shopAdress}`,
-      `Descripción: ${originalShopComments} -> ${shopComments}`,
-    ]
-      .filter((message) => message.includes("->"))
-      .join("\n");
-
-    const confirmation = window.confirm(
-      `¿Seguro que desea actualizar la tienda?\n\n${confirmationMessage}`
-    );
-
-    if (confirmation) {
-      const formData = new FormData();
-      formData.append("file", selectedFile);
-      formData.append("shopName", shopName);
-      formData.append("shopTell", shopTell);
-      formData.append("shopMail", shopMail);
-      formData.append("shopAdress", shopAdress);
-      formData.append("shopOwner", shopOwner);
-      formData.append("shopComments", shopComments);
-      formData.append("shopId", shopId);
-
-      Axios.put(
-        "http://localhost:3000/updateShop",
-        formData,
-        {}
-      ).then(() => {
-        getShops();
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "Tienda actializada",
-          showConfirmButton: false,
-          timer: 1500,
-        });
-        limpiarCampos();
-      });
-    } else {
-      // El usuario ha hecho clic en "Cancelar"
-      Swal.fire({
-        icon: "error",
-        title: "Actualizacion cancelada",
-      });
-    }
-  };
-
-  const deleteShop = (ShopId) => {
-    const confirmation = window.confirm(
-      "¿Seguro que desea eliminar la tienda?"
-    );
-
-    if (confirmation) {
-      Axios.put(
-        `http://localhost:3000/deleteShop/${ShopId}`
-      ).then(() => {
-        alert("Tienda eliminada");
-        limpiarCampos();
-        getShops();
-      });
-    } else {
-      // El usuario ha hecho clic en "Cancelar"
-      alert("Eliminación cancelada");
-    }
-  };
-
-  const deleteProducts = (ShopId) => {
-    Axios.put(
-      `http://localhost:3000/deleteProducts/${ShopId}`
-    ).then(() => {
-      limpiarCampos();
-      getShops();
-    });
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
   };
 
   const limpiarCampos = () => {
@@ -144,63 +64,23 @@ function CreateShop() {
     setShopMail("");
     setShopAdress("");
     setShopComments("");
-    setShopId("");
-    setEditar(false);
-  };
-
-  const CancelarUpdate = () => {
-    limpiarCampos();
-    setEditar(false);
-  };
-
-  const editarTienda = (val) => {
-    setEditar(true);
-    console.log("soy valshop id de la funcion", val.shopId);
-    setShopId(val.shopId);
-    setShopName(val.shopName);
-    setShopTell(val.shopTell);
-    setShopMail(val.shopMail);
-    setShopAdress(val.shopAdress);
-    setShopComments(val.shopComments);
-  };
-
-  const getShops = (shopOwner) => {
-    Axios.get(
-      `http://localhost:3000/shopsListCreateShops/${shopOwner}`
-    ).then((response) => {
-      setShopsList(response.data);
-      console.dir(response.data);
-    });
-  };
-  
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]); // Cambiado de file a selectedFile
-  };
-
-  const GoToOrdersManagment = (shopId) => {
-    setGlobalShopId(shopId);
   };
 
   useEffect(() => {
-    setShopOwner(Cookies.get("userId"));
-    console.log("soy el shop owner", shopOwner);
-    console.log("soy el global shop id", globalShopId);
-    getShops(shopOwner); // Pasar shopOwner como argumento
-  }, [shopOwner]);
+    // Aquí se pueden agregar las operaciones de inicialización necesarias
+  }, []);
 
   return (
     <>
       <Header />
-      <div className="container ">
+      <div className="container">
         <div className="card">
           <div className="card-header">
             <h2 className="title-create-store">Creación De Tiendas</h2>
           </div>
           <div className="card-body">
             <div className="label-store">
-              <span id="basic-addon1">
-                Nombre de la tienda:
-              </span>
+              <span id="basic-addon1">Nombre de la tienda:</span>
               <input
                 type="text"
                 value={shopName}
@@ -210,12 +90,13 @@ function CreateShop() {
                 className="input-store"
                 placeholder="Nombre la tienda"
               />
+              {!validateShopName(shopName) && (
+                <p className="error-message">El nombre debe comenzar con mayúscula</p>
+              )}
             </div>
 
             <div className="label-store">
-              <span id="basic-addon1">
-                Teléfono:
-              </span>
+              <span id="basic-addon1">Teléfono:</span>
               <input
                 type="tel"
                 value={shopTell}
@@ -225,12 +106,13 @@ function CreateShop() {
                 className="input-store"
                 placeholder="315 000 0000"
               />
+              {!validatePhoneNumber(shopTell) && (
+                <p className="error-message">El teléfono debe tener 10 dígitos</p>
+              )}
             </div>
 
             <div className="label-store">
-              <span id="basic-addon1">
-                Correo electrónico:
-              </span>
+              <span id="basic-addon1">Correo electrónico:</span>
               <input
                 type="email"
                 value={shopMail}
@@ -240,12 +122,13 @@ function CreateShop() {
                 className="input-store"
                 placeholder="correo@gmail.com"
               />
+              {!validateEmail(shopMail) && (
+                <p className="error-message">Correo electrónico inválido</p>
+              )}
             </div>
 
             <div className="label-store">
-              <span id="basic-addon1">
-                Dirección:
-              </span>
+              <span id="basic-addon1">Dirección:</span>
               <input
                 type="text"
                 value={shopAdress}
@@ -258,9 +141,7 @@ function CreateShop() {
             </div>
 
             <div className="label-store">
-              <span id="basic-addon1">
-                Descripción:
-              </span>
+              <span id="basic-addon1">Descripción:</span>
               <textarea
                 type="text"
                 value={shopComments}
@@ -281,103 +162,21 @@ function CreateShop() {
                 id="file"
                 name="file"
                 onChange={handleFileChange}
-                style={{ display: "none" }} // Oculta el input de tipo archivo
+                style={{ display: "none" }}
               />
               {selectedFile && (
                 <div className="file-info">
                   <p className="result-select-img">{selectedFile.name}</p>
-                  {/* Puedes agregar más información sobre el archivo si lo deseas */}
                 </div>
               )}
             </div>
           </div>
           <div className="card-footer text-body-secondary d-flex justify-content-center">
-            {editar ? (
-              <div className="w-52 d-flex justify-content-between">
-                <button onClick={updateShop} className="btn-update-store">
-                  Actualizar
-                </button>
-                <button onClick={CancelarUpdate} className="btn-cancel-store">
-                  Cancelar
-                </button>
-              </div>
-            ) : (
-              <button onClick={addShop} className="btn-new-store">
-                Registrar
-              </button>
-            )}
+            <button onClick={addShop} className="btn-new-store">
+              Registrar
+            </button>
           </div>
         </div>
-
-      {/*  <table className="table table-hover mt-12">
-          <thead className="table-titles">
-            <tr>
-              <th scope="col">Nombre</th>
-              <th scope="col">Teléfono</th>
-              <th scope="col">Correo Electrónico</th>
-              <th scope="col">Dirección</th>
-              <th scope="col">Descripcion</th>
-              <th scope="col">Acciones</th>
-            </tr>
-          </thead>
-          <tbody className="table-body">
-            {shopsList.map((val, key) => (
-              <tr key={val.shopId} className="">
-                <td>{val.shopName}</td>
-                <td>{val.shopTell}</td>
-                <td>{val.shopMail}</td>
-                <td>{val.shopAdress}</td>
-                <td>{val.shopComments}</td>
-                <td>
-                  <div
-                    className="flex items-center justify-center gap-1"
-                    role="group"
-                    aria-label="Basic example"
-                  >
-                    <button
-                      type="button"
-                      onClick={() => {
-                        console.log("soy val shop id", val.shopId);
-                        editarTienda(val);
-                      }}
-                      className="btn-edit-store"
-                    >
-                      Editar
-                    </button>
-                    <button
-                      type="button"
-                      onClick={() => {
-                        setGlobalShopId(val.shopId);
-                        navigate("/UpdateShop");
-                      }}
-                      className="btn-link-store"
-                    >
-                      Productos
-                    </button>
-                    <button
-                      onClick={() => {
-                        GoToOrdersManagment(val.shopId);
-                        navigate("/OrdersManagment");
-                      }}
-                      className="btn-delivery-store"
-                    >
-                      Entregas
-                    </button>
-                    <button
-                      type="button"
-                      onClick={() => {
-                        deleteProducts(shopOwner), deleteShop(val.shopId);
-                      }}
-                      className="btn-delete-store"
-                    >
-                      Eliminar
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-                    </table> */}
       </div>
       <Footer />
     </>
